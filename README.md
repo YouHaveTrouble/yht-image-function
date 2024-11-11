c3afba2fb491c53de46527bc4e4ef8a832da3455
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-11-11 12:21:52 +0100

    I think it works now

---
+++ README.md
@@ -0,0 +1,5 @@
+# YHT's Image functions
+
+Collection of various image operations allowing for mass image processing.
+
+All processing takes place in the browser, so no images are uploaded to any server.
---
+++ src/util/ImageExtenstion.ts
@@ -0,0 +1,11 @@
+export const FileMimeType: {[key:string]: string} = {
+  'image/x-jg': 'art',
+  'image/bmp': 'dib',
+  'image/gif': 'gif',
+  'image/jpeg': 'jpg',
+  'image/pict': 'pict',
+  'image/png': 'png',
+  'image/webp': 'webp',
+  'image/svg+xml': 'png',
+  'application/svg+xml': 'png',
+};
